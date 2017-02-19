b5a7e0b73032c19d426123e47275496aa07024ae
André Pascoal Bento <andrebento1995@sapo.pt>
2017-02-19 03:43:00 +0000

    Some code #3 + e-book

--- decisions_making.go
+++ decisions_making.go
@@ -34,7 +34,7 @@ func main() {
         }
     */
     if(true) {
-        fmt.Printfln("Passei em if(true)")
+        fmt.Println("Passei em if(true)")
     } else {
         fmt.Println("Passei em else")
     }
@@ -51,11 +51,11 @@ func main() {
     /*      switch statement
         switch(boolean-expression or integral type){
             case boolean-expression or integral type  :
-            statement(s);      
+                statement(s);      
             case boolean-expression or integral type  :
-            statement(s); 
+                statement(s); 
             default : #Optional
-            statement(s);
+                statement(s);
         }
     */
     var name string = "andrebento"
@@ -65,8 +65,37 @@ func main() {
         case "andreacruz":
             fmt.Println("variable name is andreacruz")
         default:
-            fmt.Println("none of casesvariable name")
+            fmt.Println("none of cases is matching variable name")
     }
 
+    /*      select statement
+        select {
+            case communication_clause:
+                statement(s);
+            case communication_clause:
+                statement(s);
+            default: #Optional
+                statement(s);
+        }
+    */
+    var c1, c2, c3 chan int
+    var i1, i2 int
+    select {
+        case i1 = <-c1:
+            fmt.Printf("received ", i1, " from c1\n")
+        case c2 <- i2:
+            fmt.Printf("sent ", i2, " to c2\n")
+        case i3, ok := (<-c3):
+            if ok {
+                fmt.Printf("received ", i3, " from c3\n")
+            } else {
+                fmt.Printf("c3 is closed\n")
+            }
+        default:
+            fmt.Printf("no communication\n")
+    }
 
+    // Just some variable declaration example
+    v1 := 10
+    fmt.Printf("%d", v1)
 }
